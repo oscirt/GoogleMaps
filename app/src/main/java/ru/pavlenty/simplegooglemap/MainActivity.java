d51d95a87526b5efc37809b2ac07e57b545e3fc8
oscirt <a@example.com>
2021-11-29 18:05:02 +0300

    Initial commit

--- app/src/main/java/ru/pavlenty/simplegooglemap/MainActivity.java
+++ app/src/main/java/ru/pavlenty/simplegooglemap/MainActivity.java
@@ -3,6 +3,7 @@ package ru.pavlenty.simplegooglemap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,7 +11,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity  implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,10 @@ public class MainActivity extends AppCompatActivity  implements OnMapReadyCallba
 
     @Override
     public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        map.addMarker(new MarkerOptions().position(new LatLng(2, 2)).title("Marker1"));
+        map.addMarker(new MarkerOptions().position(new LatLng(30, 32)).title("Marker2"));
+        map.addMarker(new MarkerOptions().position(new LatLng(14, 14)).title("Marker3"));
+        map.addMarker(new MarkerOptions().position(new LatLng(75, 75)).title("Marker4"));
+        map.addMarker(new MarkerOptions().position(new LatLng(26, 26)).title("Marker5"));
     }
 }
